40c7f6a6c3d4c13df80dba4f82ba7446d913a198
debopom-dey <71516417+debopom-dey@users.noreply.github.com>
2020-12-18 22:18:33 +0530

    Add files via upload

---
+++ Drop.js
@@ -0,0 +1,27 @@
+class Drop{
+    constructor(x,y){
+    var options={
+        restitution:0.1,
+        friction:0.001
+    }
+    
+    this.rain=Bodies.circle(x,y,5,options);
+    this.r=5;
+    World.add(world,this.rain);
+    }
+    
+    display(){
+    var pos= this.rain.position;
+    fill("blue")
+    ellipseMode(CENTER);
+    ellipse(pos.x,pos.y,this.r,this.r)
+    pop();
+    
+    }
+    update(){
+      if(this.rain.position.y>height){
+          Matter.Body.setPosition(this.rain,{x:random(0,400),y:random(0,400)})
+      }
+
+    }
+    }
